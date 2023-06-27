81e010d073a8c77f00486bdb0c8e5b8a0111fd4d
sebastian lucas lopez <luxioxbox49@gmail.com>
2023-06-27 02:49:44 -0400

    arreglos

--- app/principal/contacto/page.jsx
+++ app/principal/contacto/page.jsx
@@ -8,7 +8,7 @@ export default function Index() {
 	        	<h4>Formulario de Contacto</h4>
 		    	<input className={styles.controls} type="text" name="nombre" id="nombre" placeholder="Ingrese su Nombre"/>
 		     	<input className={styles.controls} type="text" name="apellido" id="apellido" placeholder="Ingrese su Apellido"/>
-		        <input className={styles.controls} type="email" name="correo" id="correo" placeholder="Ingrese su Correo"/>
+		      <input className={styles.controls} type="email" name="correo" id="correo" placeholder="Ingrese su Correo"/>
 		        <input className={styles.controls} type="tel" name="telefono" id="telefono" placeholder="Ingrese su Número de Teléfono"/>
 		        <textarea className={styles.controls} name="mensaje" id="mensaje" placeholder="Deje su Mensaje"></textarea>
 	        	<input className={styles.botons} type="submit" value="Enviar"/>
@@ -16,6 +16,7 @@ export default function Index() {
     	</div>  
 	)
 }
+
 export async function getServerSideProps(){
   try{
     await conexionBD()
